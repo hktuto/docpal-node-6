0940f86d20f6efad6a9d49e15364bc3489665865
Sean Tsang <9394wing@gmail.com>
2025-12-19 10:49:08 +0800

    chore: Update docker-compose and configuration files
    Changes:
    - Removed version specification from docker-compose.dev.yml.
    - Updated Nuxt configuration to disable blob storage.
    - Changed docker-compose commands in package.json to use the new syntax.
    - Updated pnpm-lock.yaml to reflect dependency changes and optimizations.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@element-plus/nuxt','@nuxthub/core'],
   hub:{
-    blob: true,
+    blob: false,
     db: 'postgresql',
     dir: '.data'
   }
